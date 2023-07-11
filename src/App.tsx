082a4ff7d36f4d2879c6a0238006b8ae004ac825
Thiago Lira <thiago.slira@outlook.com>
2023-07-11 18:02:24 -0300

    Alterações finais

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-// import { useState } from 'react';
 import './App.css';
 import Body from './components/Body';
-// import Footer from './components/Footer';
-// import Header from './components/Header';
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <div className="root">
-      {/* <Header /> */}
       <Body />
-      {/* <Footer /> */}
     </div>
   );
 }
