4720aec36110a7bde9f50b1caef021e57cc0ea80
Thiago Lira <thiago.slira@outlook.com>
2023-07-11 17:56:05 -0300

    Projeto View Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 // import { useState } from 'react';
 import './App.css';
 import Body from './components/Body';
-import Footer from './components/Footer';
-import Header from './components/Header';
+// import Footer from './components/Footer';
+// import Header from './components/Header';
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <body className="root">
-      <Header />
+    <div className="root">
+      {/* <Header /> */}
       <Body />
-      <Footer />
-    </body>
+      {/* <Footer /> */}
+    </div>
   );
 }
 
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -8,7 +8,6 @@ function Body() {
     id: 0,
   });
   const [dataValue, setDataValue] = useState('');
-  const [errorValue, setErrorValue] = useState('');
 
   const requisitionAxios = async (
     url: string,
@@ -16,14 +15,15 @@ function Body() {
   ) => {
     if (method === 'POST') {
       try {
-        const response = await axios.post(url, {
+        const dataPost = url.includes('start') ? {
           nome: inputsValue.name,
-        });
+        } : {
+          codCandidato: Number(inputsValue.id),
+        };
+        const response = await axios.post(url, dataPost);
         setDataValue(response.data);
-        console.log(response.data);
       } catch (error: any) {
-        setErrorValue(error.response.data);
-        console.error(errorValue);
+        setDataValue(error.response.data);
       }
     } else {
       try {
@@ -31,13 +31,20 @@ function Body() {
         setDataValue(response.data);
         console.log(response.data);
       } catch (error: any) {
-        setErrorValue(error.response.data);
-        console.error(errorValue);
+        setDataValue(error.response.data);
       }
     }
   };
 
+  const resetInputsValue = () => {
+    setInputsValue({
+      name: '',
+      id: 0,
+    });
+  };
+
   const handleButtonClick = async (event: any) => {
+    resetInputsValue();
     switch (event.target.name) {
       case 'start':
         requisitionAxios(
@@ -87,70 +94,93 @@ function Body() {
 
   return (
     <div>
-      <section>
+      <section className="initiate">
         <h2>Iniciar Processo</h2>
         <button
           onClick={ handleButtonClick }
           name="start"
+          disabled={ inputsValue.name.length < 3 }
+          className="button-start"
         >
           Iniciar Processo
         </button>
-        <input
-          type="text"
-          onChange={ handleInputChange }
-          name="name"
-        />
+        <label htmlFor="input-name">
+          <input
+            id="input-name"
+            type="text"
+            onChange={ handleInputChange }
+            name="name"
+            minLength={ 3 }
+            value={ inputsValue.name }
+            placeholder="Nome do Candidato"
+          />
+        </label>
       </section>
-      <section>
+      <section className="actions">
         <h2>Ações</h2>
-        <input
-          type="number"
-          onChange={ handleInputChange }
-          name="id"
-        />
         <button
           onClick={ handleButtonClick }
           name="schedule"
+          className="button-schedule"
         >
           Marcar Entrevista
         </button>
         <button
           onClick={ handleButtonClick }
           name="disqualify"
+          className="button-disqualify"
         >
           Desqualificar Candidato
         </button>
         <button
           onClick={ handleButtonClick }
           name="approve"
+          className="button-approve"
         >
           Aprovar Candidato
         </button>
       </section>
-      <section>
+      <section className="input-id-section">
+        <label htmlFor="input-codCandidate">
+          <input
+            id="input-codCandidate"
+            type="number"
+            onChange={ handleInputChange }
+            name="id"
+            value={ inputsValue.id }
+            min={ 0 }
+          />
+        </label>
+      </section>
+      <section className="data">
         <h2>Dados</h2>
-        <input
-          type="number"
-          onChange={ handleInputChange }
-          name="id"
-        />
         <button
           onClick={ handleButtonClick }
           name="status/candidate"
+          disabled={ inputsValue.id < 0 }
+          className="button-status"
         >
           Verificar Status
         </button>
         <button
           onClick={ handleButtonClick }
           name="approved"
+          className="button-approved"
         >
           Ver Aprovados
         </button>
       </section>
-      <section>
+      <section className="response">
         <h2>Resposta</h2>
-        <p>{ dataValue }</p>
-        { errorValue && <p>{ errorValue }</p>}
+        {Array.isArray(dataValue) ? (
+          <ul>
+            {dataValue.map((item, index) => (
+              <li key={ index }>{item}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>{dataValue}</p>
+        )}
       </section>
     </div>
   );
